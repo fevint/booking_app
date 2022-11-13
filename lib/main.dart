b58625220e3ea5cfdf627ea1884ef909e188538c
fevint <fevint.k@gmail.com>
2022-11-13 14:28:35 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:booking_apps/presentation/pages/onboarding/onboarding_view.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Booking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+      ),
+      home: OnboardingPage(),
+    );
+  }
+}
---
+++ lib/presentation/pages/onboarding/component/onboarding_title.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingTitle extends StatelessWidget {
+  const OnboardingTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
